561b096b21ebc7ddea5bdb7d5c6504ff41b63009
paezha <paezha@users.noreply.github.com>
2021-12-17 22:06:13 +0000

    Built site for macdown: 0.1.0@8795400

--- reference/figures/hex_creator.R
+++ reference/figures/hex_creator.R
@@ -1,16 +1,38 @@
+## code to create hex sticker
+
+library(ggplot2)
+library(here)
+library(magick)
 library(hexSticker)
+library(showtext)
 
-# https://pixabay.com/illustrations/graduation-hats-throw-graduation-4906359/
-cap <- "images/happy_cap.png"
+font_add_google("Roboto Condensed")
 
-sticker(cap,
-  package = "thesisdown",
-  p_family = "Aller_Rg",
-  p_size = 7, p_color = "#5B5040", # p_x = 4, p_y = 1.4,
-  s_x = 1, s_y = .8, s_width = 0.75, s_height = 0.75,
-  h_fill = "#FAECC7", h_color = "#5B5040",
-  filename = "thesisdown_hex.png",
-  url = "thesisdown.rbind.io"
-)
+thesis_svg <- image_read_svg('https://brand.mcmaster.ca/app/uploads/2019/04/icon-bookmark_svg.svg', width = 900)
 
-# Final image was revised in Adobe Illustrator
+hp <- thesis_svg %>%
+  #image_crop("143x350+80") %>%
+  image_ggplot()
+
+
+hs <- sticker(hp,
+              package="macdown",
+              h_fill="#7A003C",
+              h_color="#FDBF57",
+              p_size=6,
+              p_x = 0.98,
+              p_y = 0.75,
+              p_family = "Roboto Condensed",
+              p_color = "#7A003C",
+              h_size = 2,
+              s_x=0.95,
+              s_y=1,
+              s_width=2.5,
+              s_height=2.5,
+              url = "paezha.github.io/macdown",
+              u_color = "white",
+              u_family = "Roboto Condensed",
+              u_x = 1.05,
+              u_y = 0.15,
+              u_size = 2,
+              filename = paste0(here(), "/man/figures/macdown.png"))
